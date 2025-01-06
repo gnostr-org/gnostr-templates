de47720bdf642e6786b9d58fe5ccf505d175ea48
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2025-01-06 14:43:23 -0500

    diff --git a/nostui-template/template/src/tui.rs b/nostui-template/template/src/tui.rs index 39d7d96..5a8b9da 100644 --- a/nostui-template/template/src/tui.rs +++ b/nostui-template/template/src/tui.rs @@ -100,13 +100,11 @@ impl Tui {          self      }
    -    async fn sleep_then_print(timer: i32) {
    -        log::info!("tui:>>>>>----------->>>Start timer {}.", timer);
    -
    -        // No .await here!
    +    async fn tui_async_entrypoint(timer: i32) {
    +        //life cycle limited to initialization
    +        log::info!("tui_async_entrypoint:{}.", timer);
             std::thread::sleep(Duration::from_secs(1));
    -
    -        log::info!("tui:>>>>>----------->>>Timer {} done.", timer);
    +        log::info!("tui_async_entrypoint:{}.", timer);
         }
    
    @@ -120,17 +118,9 @@ impl Tui {
             let _cancellation_token = self.cancellation_token.clone();
             let _event_tx = self.event_tx.clone();
             //async
    -        //self.task = tokio::join!(
    -          //  Self::sleep_then_print(1),
    -          //  Self::sleep_then_print(2),
    -          //  Self::sleep_then_print(3),
    -        //);
    -        //async
             self.task = tokio::spawn(async move {
                 //async
    -            Self::sleep_then_print(1).await;
    -            Self::sleep_then_print(2).await;
    -            Self::sleep_then_print(3).await;
    +            Self::tui_async_entrypoint(0).await;
                 let mut reader = crossterm::event::EventStream::new();
                 let mut tick_interval = tokio::time::interval(tick_delay);
                 let mut render_interval = tokio::time::interval(render_delay);
    @@ -253,6 +243,7 @@ impl Tui {
    
         //async
         pub async fn next(&mut self) -> Option<Event> {
    +        //no Self::tui_async_entrypoint(0).await;
             self.event_rx.recv().await
         }
     }

--- nostui-template/template/src/tui.rs
+++ nostui-template/template/src/tui.rs
@@ -100,13 +100,11 @@ impl Tui {
         self
     }
 
-    async fn sleep_then_print(timer: i32) {
-        log::info!("tui:>>>>>----------->>>Start timer {}.", timer);
-    
-        // No .await here!
+    async fn tui_async_entrypoint(timer: i32) {
+        //life cycle limited to initialization
+        log::info!("tui_async_entrypoint:{}.", timer);
         std::thread::sleep(Duration::from_secs(1));
-    
-        log::info!("tui:>>>>>----------->>>Timer {} done.", timer);
+        log::info!("tui_async_entrypoint:{}.", timer);
     }
 
 
@@ -120,17 +118,9 @@ impl Tui {
         let _cancellation_token = self.cancellation_token.clone();
         let _event_tx = self.event_tx.clone();
         //async
-        //self.task = tokio::join!(
-          //  Self::sleep_then_print(1),
-          //  Self::sleep_then_print(2),
-          //  Self::sleep_then_print(3),
-        //);
-        //async
         self.task = tokio::spawn(async move {
             //async
-            Self::sleep_then_print(1).await;
-            Self::sleep_then_print(2).await;
-            Self::sleep_then_print(3).await;
+            Self::tui_async_entrypoint(0).await;
             let mut reader = crossterm::event::EventStream::new();
             let mut tick_interval = tokio::time::interval(tick_delay);
             let mut render_interval = tokio::time::interval(render_delay);
@@ -253,6 +243,7 @@ impl Tui {
 
     //async
     pub async fn next(&mut self) -> Option<Event> {
+        //no Self::tui_async_entrypoint(0).await;
         self.event_rx.recv().await
     }
 }
